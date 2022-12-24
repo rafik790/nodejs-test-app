6aa138493b728fd18156c597f27d95ce455c8ffa
Rafik Mondal <RafikM@interrait.com>
2022-12-24 13:40:46 +0530

    Added new configuration

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express =require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app= express();
-const port =  80;
+const port = process.env.PORT || 3000;
+const corsOptions = {
+    "origin": "*",
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to Test Demo Project.." });
